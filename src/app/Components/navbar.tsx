aa794847b5384527b1de92adf603502c838da247
Mrhell01 <panchalkaran967@gmail.com>
2024-10-24 11:06:24 +0530

    Styled Navbar

--- src/app/Components/navbar.tsx
+++ src/app/Components/navbar.tsx
@@ -11,23 +11,19 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="flex justify-between items-center p-3 navbar bg-[#2d2e2e]">
-      <div className="text-white">
-        <Link href="/" className="text-white ml-12">Logo</Link>
+    <nav className="flex font-serif justify-between items-center p-3 navbar bg-white text-blue-900 border-blue-200 shadow-md border-b">
+      <div className="">
+        <Link href="/" className="text-2xl ml-10 font-semibold font-serif border-b-2 border-blue-400">AM TOOL</Link>
       </div>
-      <Link href="/workflow" className="text-white pl-40">WorkFlow</Link>
 
-      {/* Search Bar */}
       <div className="flex items-center gap-4">
-        {/* Search Button */}
+
         <button
           onClick={toggleSearch}
-          className="bg-gray-600 text-white py-2 px-4 rounded"
+          className="bg-[#eef6ff] border-blue-400 border-2 text-blue-900  py-2 px-4 rounded"
         >
           Search
         </button>
-
-        {/* Search Bar with Animation */}
         <div
           className={`relative transition-all duration-500 ${isSearchVisible ? 'max-w-xs' : 'max-w-0'} overflow-hidden`} // Slower animation
         >
@@ -41,20 +37,21 @@ const Navbar: React.FC = () => {
           <FaSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 text-black" />
         </div>
 
+        <Link href="/workflow" className="hover:border-b-2 hover:border-blue-400">WorkFlow</Link>
         {/* Links */}
         
-        <Link href="/about" className="text-white">About</Link>
-        <Link href="/contact" className="text-white">Contact Us</Link>
+        <Link href="/about" className="hover:border-b-2 hover:border-blue-400">About</Link>
+        <Link href="/contact" className="hover:border-b-2 hover:border-blue-400">Contact Us</Link>
 
         {/* Help Link with Icon */}
-        <Link href="/help" className="text-white flex items-center gap-2">
+        <Link href="/help" className="hover:border-b-2 hover:border-blue-400 flex items-center gap-2">
           <FaQuestionCircle />
           Help
         </Link>
 
         {/* Profile Circle */}
-        <div className="w-10 h-10 rounded-full bg-gray-500 flex items-center justify-center">
-          <span className="text-white">P</span>
+        <div className="w-10 h-10 rounded-full bg-[#eef6ff] border-2 border-blue-400 flex items-center justify-center">
+          <span className="">KP</span>
         </div>
       </div>
     </nav>
